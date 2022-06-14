b91d7250d4d6285b56220df14dd848120cc36b7a
Mirlan <mika.sootbaev1309@gmail.com>
2022-06-14 19:01:06 +0600

    Доделать кнопку, меняющий порядок

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Blog(models.Model):
+	text = models.TextField()
+	title = models.CharField(max_length=200, unique=True)
+	created_at = models.DateTimeField(auto_now=True)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.title
+
+	class Meta:
+		ordering = ['-id']
--- blogs/views.py
+++ blogs/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from .models import Blog
 
 def blog_list(request):
-	return render(request, 'blogs/blog_list.html')
+	ordering = request.GET.get('ordering', '-id')
+	blogs = Blog.objects.all()
+	if ordering == 'id':
+		blogs = blogs.order_by('id')
+		ordering = '-id'
+	else:
+		blogs = blogs.order_by('id')
+		ordering = '-id'
+	context = {
+		'blogs': blogs,
+		'ordering': ordering
+	}
+	return render(request, 'blogs/blog_list.html', context)
 
 @login_required()
 def blog_create(request):
 	return render(request, 'blogs/blog_create.html')
+
