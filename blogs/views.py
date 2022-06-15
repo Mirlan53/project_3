3dedafcc3cbdcf0998e448bd48a9813a3a87d912
Mirlan <mika.sootbaev1309@gmail.com>
2022-06-15 19:56:53 +0600

    Сделали фильтр

--- blogs/views.py
+++ blogs/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Blog
+from django.db.models import Q
 
 def blog_list(request):
 	ordering = request.GET.get('ordering', '-id')
 	blogs = Blog.objects.all()
+	q = request.GET.get('q')
+	q_user = request.GET.get('q_user')
+	if q or q_user:
+		blogs = Blog.objects.filter((Q(title__icontains=q) | Q(text__icontains=q)) & Q(author__username__icontains=q_user)).distinct()
 	if ordering == 'id':
 		blogs = blogs.order_by('id')
 		ordering = '-id'
@@ -13,7 +18,9 @@ def blog_list(request):
 		ordering = '-id'
 	context = {
 		'blogs': blogs,
-		'ordering': ordering
+		'ordering': ordering,
+		'q': q,
+		'q_user': q_user
 	}
 	return render(request, 'blogs/blog_list.html', context)
 
